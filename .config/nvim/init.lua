6f2072a8105db424aa6ce4fd2926616a5d747c17
voluntas <mr.voluntas@proton.me>
2025-10-02 00:45:47 +0100

    removing nvim, adding back in doom

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocommands")
-require("config.lazy")
-require("config.colourscheme")
-require("config.lsp")
--- .config/nvim/lua/plugins/rasmus.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "kvrohit/rasmus.nvim",
-  lazy = false, -- load on startup
-  priority = 1000, -- ensure it loads before other colorschemes
-}

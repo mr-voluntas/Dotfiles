06211bc70c8f49a0a5c98558f1951584724d1060
voluntas <mr.voluntas@proton.me>
2026-02-14 15:57:01 +0000

    updates

--- install.sh
+++ install.sh
@@ -9,6 +9,4 @@ DOTFILES_DIR="$HOME/dotfiles"
 mkdir -p ~/.config
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/.zprofile" "$HOME/.zprofile"
-ln -sf "$DOTFILES_DIR/.git" "$HOME/.git"
-ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DOTFILES_DIR/.config/nvim" "$HOME/.config/nvim"
